96b399185caa54a68cee474982ccf7eeb2b47bee
Amirul Faqih <fakiiahmad001@gmail.com>
2025-02-25 23:47:50 +0300

    add comment to clarify that ThemeProvider is a client component

--- app/provider.tsx
+++ app/provider.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
+// This is a client component so you can't use server components inside of it
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
